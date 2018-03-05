8df8a1d525f7dbb7737cdd9ff6c04182e26d1066
Samuel Brehm <samuebrehm@gmail.com>
2018-03-05 14:09:42 -0300

    Primeiro

---
+++ Capitulo01/Capitulo01/Controllers/InstituicaoController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Capitulo01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Capitulo01.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        private static IList<Instituicao> instituicoes = new List<Instituicao>()
+        {
+            new Instituicao(){
+                InstituicaoID=1,
+                Nome="UniParaná",
+                Endereco="Paraná"
+            },
+            new Instituicao(){
+                InstituicaoID=2,
+                Nome="UniSanta",
+                Endereco="Santa Catarina"
+            },
+            new Instituicao(){
+                InstituicaoID=3,
+                Nome="UniSãoPaulo",
+                Endereco="São Paulo"
+            },
+            new Instituicao(){
+                InstituicaoID=4,
+                Nome="UniSulgrandense",
+                Endereco="RionGRande do Sul"
+            },
+            new Instituicao(){
+                InstituicaoID=5,
+                Nome="UniCarioca",
+                Endereco="Rio de Janeiro"
+            }
+        };
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(instituicoes);
+        }
+    }
+}
---
+++ Capitulo01/Capitulo01/Models/Instituicao.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Capitulo01.Models
+{
+    public class Instituicao
+    {
+        public long? InstituicaoID { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+    }
+}
