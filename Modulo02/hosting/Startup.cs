5816e9c16575bf129d6c715ace326506c36214fb
Samuel Brehm <samuebrehm@gmail.com>
2018-03-05 16:58:12 -0300

    2

---
+++ Modulo02/hosting/Startup.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace hosting
+{
+
+  public class Startup
+  {
+    public void Configure(IApplicationBuilder app)
+    {
+      app.Use(async (context, next) =>{
+        context.Response.Headers.Add("Middleware", "Aprendendo");
+        await next.Invoke();
+      });
+
+      app.Run(context => context.Response.WriteAsync("Olá Mundo 2"));
+    }
+  }
+}
